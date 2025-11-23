7c85f5b722f25807d9d6dc15eb6754c1d561b75b
Arthur Vendramini Souza <vendraminiarthur2008@gmail.com>
2025-11-23 17:51:36 -0300

    EDUTEC

---
+++ script.js
@@ -0,0 +1,65 @@
+document.getElementById("btn-ir").addEventListener("click", () => {
+  document.getElementById("inicio").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+const btnMenu = document.getElementById("barras");
+const btnFechar = document.getElementById("btn-fechar");
+const sidebar = document.getElementById("sidebar");
+
+btnMenu.addEventListener("click", () => {
+  sidebar.classList.add("ativo");
+});
+
+btnFechar.addEventListener("click", () => {
+  sidebar.classList.remove("ativo");
+});
+
+let sidebarWidth = 260;
+let position = -sidebarWidth;
+let speed = 15;
+let animating = false;
+
+function abrirSidebar() {
+  if (animating) return;
+  animating = true;
+
+  function animar() {
+    position += speed;
+
+    if (position >= 0) {
+      position = 0;
+      animating = false;
+    } else {
+      requestAnimationFrame(animar);
+    }
+
+    sidebar.style.right = position + "px";
+  }
+
+  animar();
+}
+
+function fecharSidebar() {
+  if (animating) return;
+  animating = true;
+
+  function animar() {
+    position -= speed;
+
+    if (position <= -sidebarWidth) {
+      position = -sidebarWidth;
+      animating = false;
+    } else {
+      requestAnimationFrame(animar);
+    }
+
+    sidebar.style.right = position + "px";
+  }
+
+  animar();
+}
+
+btnMenu.addEventListener("click", abrirSidebar);
+btnFechar.addEventListener("click", fecharSidebar);
